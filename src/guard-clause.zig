60790776b5a6ba88bed9d7e064bc71eda6aae464
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-04-13 11:43:38 +1000

    add guard clauses

--- src/guard-clause.zig
+++ src/guard-clause.zig
@@ -3,16 +3,29 @@
 
 const std = @import("std");
 
+/// Idiomatic way to do a guard clause in zig for early return on optional
 pub fn guardAgainstOptional(num: ?u32) void {
-    // Idiomatic way to do a guard clause in zig for early return on optional
-    const my_mandator_number = num orelse return;
-    std.debug.print("My number is {d}\n", .{ my_mandator_number });
+    const my_mandatory_number = num orelse return;
+    std.debug.print("My number is {d}\n", .{ my_mandatory_number });
 }
 
-test "guardAgainstOptional" {
+/// What if I wanted to do something in the null branching path?
+/// We can use blocks since they are expressions
+pub fn guardAgainstOptionalWithBranchingLogic(num: ?u32) void {
+    const my_mandatory_number = num orelse {
+        std.debug.print("My number is optional sadge :(\n", .{});
+        return;
+    };
+    std.debug.print("My number is {d}\n", .{ my_mandatory_number });
+}
+
+test {
     const optional_num: ?u32 = null;
     const my_num: ?u32 = 42;
 
     guardAgainstOptional(optional_num);
     guardAgainstOptional(my_num);
+
+    guardAgainstOptionalWithBranchingLogic(optional_num);
+    guardAgainstOptionalWithBranchingLogic(my_num);
 }
